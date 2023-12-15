e79dc150789d331342bc4432ff335944ac3fed6b
Dushyant Rajeev Bhardwaj <dushyantbhardwaj26@gmail.com>
2023-12-16 02:30:32 +0530

    changes in auth options

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,6 +4,7 @@ import {MongoDBAdapter} from "@next-auth/mongodb-adapter";
 import clientPromise from "@/lib/mongodb";
 
 export const authOptions = {
+  secret: process.env.SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_FRONT_ID,
